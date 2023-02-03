bf655cec860541a36dcd30ac857ee2cc948e9c16
Jessi B <95951191+jessi-b@users.noreply.github.com>
2023-02-03 14:06:10 -0500

    add picker; add comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,11 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+# read S3 bucket file
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# add pick list
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+# add table from S3 bucket file
 streamlit.dataframe(my_fruit_list)
