71976401258255c7dc006c0a53364f2933bf0f1d
Jessi B <95951191+jessi-b@users.noreply.github.com>
2023-02-03 13:48:26 -0500

    Add breakfast header and item text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('Healthy Dinner')
+streamlit.header('Breakfast:')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale & Spinach Rocket Smoothie')
+streamlit.text('Hard-boiled Egg')
