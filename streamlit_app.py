293e192558e515a564317ebcc89cd3040c6a77c5
Jessi B <95951191+jessi-b@users.noreply.github.com>
2023-02-06 15:43:05 -0500

    add fruityvice response

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas;
+import requests;
 
 streamlit.title('Healthy Dinner')
 streamlit.header('Breakfast Menu')
@@ -13,9 +14,15 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# add pick list
+# pick list
 # fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # add table from S3 bucket file
 streamlit.dataframe(fruits_to_show)
+
+
+# fruityvice api response
+streamlit.header("Fruityvice Fruit Advice!")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
