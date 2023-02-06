81a77f5426d28b73c2195e7ecef7066b9d55fbfd
Jessi B <95951191+jessi-b@users.noreply.github.com>
2023-02-06 15:50:14 -0500

    add user input for fruityvice reponse

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,10 @@ streamlit.dataframe(fruits_to_show)
 
 # fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# allow user input
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "fruit_choice")
 # pretify json reponse 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # create table from response
