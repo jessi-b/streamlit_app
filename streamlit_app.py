7cf95750ba72070a360d13f618c6fd39572e8e00
Jessi B <95951191+jessi-b@users.noreply.github.com>
2023-02-08 10:29:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error import URL Error # error handling
+from urllib.error import URL Error
 
 streamlit.title('Healthy Dinner')
 streamlit.header('Breakfast Menu')
