e552a7edaccfd61d207272da286fd4443cc151df
Jessi B <95951191+jessi-b@users.noreply.github.com>
2023-02-08 11:15:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ streamlit.dataframe(fruityvice_normalized)
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 # query data
-my_cur.execute("select * from select * from pc_rivery_db.public.fruit_load_list")
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("Fruit List:")
 streamlit.dataframe(my_data_rows)
